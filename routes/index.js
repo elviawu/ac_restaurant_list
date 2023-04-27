0a09e2e8d63137d4e331db61dbc941e0495fb9f5
elviawu <elviawu@gmail.com>
2023-04-27 19:52:34 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
 // 引用express和express路由器
 const express = require('express')
 const router = express.Router()
-// 引入home模組程式碼
 const home = require('./modules/home')
-// 將網址結構符合 / 字串的 request 導向 home 模組
-router.use('/', home)
-// 引入restaurants模組程式碼
 const restaurants = require('./modules/restaurants')
-// 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
-router.use('/restaurants', restaurants)
-// 引入search模組程式碼
 const search = require('./modules/search')
-// 將網址結構符合 /search 字串的 request 導向 search 模組
+const users = require('./modules/users')
+
+router.use('/restaurants', restaurants)
 router.use('/search', search)
+router.use('/users', users)
+router.use('/', home)
+
 // 匯出路由器
 module.exports = router
