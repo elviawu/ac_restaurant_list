0592a395cceb5f501f4e7698630979166bb991ac
elviawu <elviawu@gmail.com>
2023-04-28 12:30:42 +0800

    feat: add user registration feature

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const restaurantSchema = new Schema({
+const userSchema = new Schema({
   name: {
     type: String,
     required: false
@@ -17,5 +17,5 @@ const restaurantSchema = new Schema({
     type: Date,
     default: Date.now
   },
-)
+})
 module.exports = mongoose.model('User', userSchema)
