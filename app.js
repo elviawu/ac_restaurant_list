28c1012bb940f200a10f6a4b4e9de10ae3d55ab9
elviawu <elviawu@gmail.com>
2023-04-28 13:42:16 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const session = require('express-session')
+const usePassport = require('./config/passport') // 載入設定檔，要寫在 express-session 以後
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -22,8 +23,10 @@ app.use(session({
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(routes)
 
+usePassport(app) // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`The server is running on http://localhost:${port}`)
