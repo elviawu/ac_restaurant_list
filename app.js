8b8ce825744fa3f3a55e0f9aec5794b63226712c
elviawu <elviawu@gmail.com>
2023-04-29 13:38:19 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const usePassport = require('./config/passport') // 載入設定檔，要寫在 
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -25,9 +26,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app) // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
