d3d41b3c5618c56f26faecae5235e48210e1e97b
elviawu <elviawu@gmail.com>
2023-04-30 13:31:52 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,13 +1,40 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Restaurant = require('../restaurant')
+const User = require('../user')
 const db = require('../../config/mongoose')
 const restaurantList = require('../../restaurant.json').results
+const userList = require('../../user.json')
 db.once('open', () => {
-  console.log('mongodb connected!')
-
-  Restaurant.create(restaurantList)
+  const userPromise = userList.map(seedUser => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(seedUser.password, salt))
+      .then(hash => {
+        return User.create({
+          name: seedUser.name,
+          email: seedUser.email,
+          password: hash
+        })
+          .catch(err => console.log(err))
+      })
+      .then(user => {
+        console.log('user = ', user.email)
+        const userRestaurants = seedUser.userRestaurants.map(item => {
+          return Object.assign(restaurantList[item - 1], { userId: user._id })
+        })
+        return userRestaurants
+      })
+      .then(userRestaurants => {
+        return Restaurant.create(userRestaurants)
+      })
+  })
+  Promise.all(userPromise)
     .then(() => {
-      console.log('restaurantSeeder created')
-      db.close()
+      console.log('Done !')
+      // process.exit()
     })
-    .catch((error) => console.log(error))
+    .catch(err => console.log(err))
 })
